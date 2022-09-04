4237e9c4ac4b6721907836da668d68b9d1cbc59f
BHARATH H J <48687237+bharathhj@users.noreply.github.com>
2022-09-04 05:40:15 +0530

    Create FindPivotIndex.java

---
+++ Arrays/Day 2/FindPivotIndex.java
@@ -0,0 +1,28 @@
+// T C : O ( n )
+// S C : O ( 1 )
+
+
+class Solution {
+    public int pivotIndex(int[] nums) {
+        
+        // BASE CASE
+        if(nums.length<1) return -1;
+        
+        int rightSum=0, leftSum=0;
+           
+        // GET TOTAL SUM
+        for(int i:nums) rightSum+=i;
+        
+        for(int i=0;i<nums.length;i++){
+            // DECREMENT TO GET RIGHT SUM 
+            rightSum-=nums[i];
+        
+            if(leftSum==rightSum) return i;
+            
+            // INCREMENT TO GET LEFT SUM
+            leftSum+=nums[i];
+        }
+        
+        return -1;
+    }
+}
