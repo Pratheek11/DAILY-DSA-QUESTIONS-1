91e4c0268553176975723e1a7dd6d6efbe1d92b8
BHARATH H J <48687237+bharathhj@users.noreply.github.com>
2022-09-04 06:22:39 +0530

    Create SquaresOfSortedArray.java

---
+++ Arrays/Day 2/SquaresOfSortedArray.java
@@ -0,0 +1,18 @@
+// T C : O ( n log n ) + O ( n ) ==> O ( n log n )
+// S C : O ( n )
+
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+       
+        int[] res = new int[nums.length];
+        
+        for(int i = 0;i<nums.length;i++){
+            res[i] = nums[i]*nums[i];
+        }
+        
+        Arrays.sort(res);
+        
+        return res;
+        
+    }
+}
