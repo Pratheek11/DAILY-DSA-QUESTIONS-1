fc71ba9bf20ed066a319c921e1c9f33ccacdaad5
BHARATH H J <48687237+bharathhj@users.noreply.github.com>
2022-09-08 06:40:30 +0530

    Update SetMatrixZero.java

--- Arrays/Day 5/SetMatrixZero.java
+++ Arrays/Day 5/SetMatrixZero.java
@@ -1,6 +1,48 @@
 // T C : 2 * ( M * N ) === O ( M * N )
 // S C : O ( 1 )
 
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        
+        // T C : 2 * ( M * N ) === O ( M * N )
+        // S C : O ( 1 )
+        
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int col0 = 1;
+        
+        // MAP THE COLUMN AND ROW WHICH HAS TO BE ZERO
+        for(int i = 0; i < rows ; i++){
+            // Condition for 0th Column
+            if( matrix[i][0] == 0 ){
+                col0 = 0;
+            }
+            
+            for(int j = 1 ; j < cols ; j++){
+                if( matrix[i][j] == 0){
+                    matrix[i][0] = matrix[0][j] = 0;
+                }
+            }
+        }
+        
+        // SET ZERO TO EVERY CELLS WITH HELP OF MAP
+        for(int i = rows -1; i >= 0; i--){
+            for(int j = cols - 1; j >= 1; j--){
+                if( matrix[i][0] == 0 || matrix[0][j] == 0){
+                    matrix[i][j] = 0;
+                }
+            }
+            if( col0 == 0 ){
+                matrix[i][0] = 0;
+            }
+        }
+        
+    }
+}
+
+// ==================================================
+//         MORE DETAILED WAY OF SAME APPROACH
+// ==================================================
 
 class Solution {
     public void setZeroes(int[][] matrix) {
