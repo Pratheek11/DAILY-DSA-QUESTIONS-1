4fa60232b43506e6dcbb1d4504a016ba3ecea9df
BHARATH H J <48687237+bharathhj@users.noreply.github.com>
2022-09-09 06:08:06 +0530

    Create BestTimeToSellStock1.java

---
+++ Arrays/Day 1/BestTimeToSellStock1.java
@@ -0,0 +1,23 @@
+// T C : O ( n )
+// S C : O ( n )
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int min = Integer.MAX_VALUE;
+        int profit = 0;
+        
+        for( int i = 0; i < prices.length ; i++ ){
+            if( prices[i] < min ){
+                min = prices[i];
+            }
+            
+            int currProfit = prices[i] - min;
+            
+            if( currProfit > profit ){
+                profit = currProfit;
+            }
+        }
+        
+        return profit;
+    }
+}
